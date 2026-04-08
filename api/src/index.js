7ecd5858af609e49a7074d8c3d5c6be6e8d78076
pradashades333 <alexdreve03@gmail.com>
2026-04-08 19:50:41 +0200

    added scores

--- api/src/index.js
+++ api/src/index.js
@@ -1,13 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
+const imagesRouter = require('./routes/images');
+
 const app = express();
 
 app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:5174']
 }));
 app.use(express.json());
-
+app.use("/images", imagesRouter);
+app.use("/scores", scoresRouter);
 
 app.get("/", (req, res) => {
   res.json({ message: "Wheres waldo is running" });
---
+++ api/src/routes/scores.js
@@ -0,0 +1,6 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/scoresController");
+
+router.get('/:imageId', controller.getLeaderboard)
+router.post('/', controller.saveScore)
