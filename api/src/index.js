83268026756fc9c90239edcd1694566e072c444f
pradashades333 <alexdreve03@gmail.com>
2026-04-08 18:53:54 +0200

    added index

---
+++ api/src/index.js
@@ -0,0 +1,24 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors({
+  origin: ['http://localhost:5173', 'http://localhost:5174']
+}));
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.json({ message: "Wheres waldo is running" });
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ error: err.message });
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
