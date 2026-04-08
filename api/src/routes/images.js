eee41ea259a97605b25a4c30c9cc0d280641dbe1
pradashades333 <alexdreve03@gmail.com>
2026-04-08 19:39:21 +0200

    added images routes

---
+++ api/src/routes/images.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/imagesController");
+
+router.get('/', controller.getAllImages)
+router.get('/:id/characters', controller.getImage)
+
+router.post('/:id/check', controller.checkClick)
