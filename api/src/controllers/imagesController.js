85f3d713d655549cf13175aeaeb456ad2081ab4a
pradashades333 <alexdreve03@gmail.com>
2026-04-09 18:11:38 +0200

    finished images controller

--- api/src/controllers/imagesController.js
+++ api/src/controllers/imagesController.js
@@ -12,13 +12,25 @@ module.exports = {
 
     getImage: async (req, res, next) => {
         try {
-            const image = await prisma.image.findUnique({
-            where: { id: parseInt(req.params.id) },include: {characters: {select: { id: true, name: true }}}});
+            const image = await prisma.image.findUnique({where: { id: parseInt(req.params.id) },include: {characters: {select: { id: true, name: true }}}});
             res.json(image);
         } catch (err) {
             next(err)
         }
     },
 
+
+    checkClick: async (req, res, next) => {
+        try {
+            const {x,y} = req.body
+            const {characterId} = req.body
+            const cord = await prisma.character.findUnique({ where: { id: parseInt(characterId) } })
+            const minDist = 100;
+            const isCorrect = Math.abs(x - cord.x) < minDist && Math.abs(y - cord.y) < minDist;
+            res.json(isCorrect)
+        } catch(err) {
+            next(err)
+        }
+    }
 
 }
